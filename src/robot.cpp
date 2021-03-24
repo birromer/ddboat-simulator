57ff7da1e86f00a3d03b3e254b6d6ed5b971cfdd
Bernardo <hummescmpa@gmail.com>
2021-03-24 16:47:18 +0100

    started robot cpp, up to integration and callback

---
+++ src/robot.cpp
@@ -0,0 +1,127 @@
+#include "ros/ros.h"
+#include "std_srvs/Trigger.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Twist.h"
+#include "tf/tf.h"
+#include <math.h>
+#include "visualization_msgs/Marker.h"
+
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#define P1 1
+#define P2 1
+#define P3 1
+
+double cmd_r, cmd_l;
+
+void cmd_r_Callback(const geometry_msgs::Twist::ConstPtr& msg) {
+  ROS_INFO("received command right: [%f]", msg->data);
+  cmd_r = msg->data;
+}
+
+void cmd_l_Callback(const geometry_msgs::Twist::ConstPtr& msg) {
+  ROS_INFO("received command left: [%f]", msg->data);
+  cmd_l = msg->data;
+}
+
+void integration_euler(double &x1, double &x2, double &x3, double &x4, double u1, double dt) {
+  x1 = x1 + dt * (x4*cos(x3));
+  x2 = x2 + dt * (x4*sin(x3));
+  x3 = x3 + dt * (P1*(u1-u2));
+  x4 = x4 + dt * (P2*(u1+u2) + P3*x4*abs(x4));
+}
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "boat_simulator");  // initialization of the node
+  ros::NodeHandle n;  // access to the node
+
+  ros::Rate loop_rate(25);  // 25Hz frequency
+
+  double dt = 0.005;  // time step
+
+  double u1 = cmd_r;
+  double u2 = cmd_l;
+
+  double x1, x2, x3, x4;
+
+  // read initial values from the launcher
+  n.param<double>("pos_x_init", x1, 0);
+  n.param<double>("pos_y_init", x2, 0);
+  n.param<double>("pos_th_init", x3, 0);
+  n.param<double>("pos_v_init", x4, 0);
+
+  // subscriber to z inputs from outside
+  ros::Subscriber z_sub = n.subscribe("z_u1", 1000, z_Callback);
+
+  // published of the robot's state
+  ros::Publisher state_pub = n.advertise<geometry_msgs::PoseStamped>("state_boat", 1000);
+
+  // publisher for the 3d visualization
+  ros::Publisher vis_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 0);
+
+  // transform broadcaster between the map and the robot
+  std::string ns = ros::this_node::getNamespace();
+
+  tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
+
+  transformStamped.header.frame_id = "map";
+  transformStamped.child_frame_id = ns.substr(1,5);
+
+  while (ros::ok()) {
+    u1 = z;
+    integration_euler(x, y, theta, u1, dt);
+
+    geometry_msgs::PoseStamped msg;
+    msg.pose.position.x = x;
+    msg.pose.position.y = y;
+    msg.pose.position.z = 0;  // assumed to be constant 0 for now
+
+    tf::Quaternion q;
+    q.setRPY(0, 0, theta);  // roll, pitch, yaw - theta normally pitch, but i think it means yaw now
+    tf::quaternionTFToMsg(q, msg.pose.orientation);
+
+    msg.header.stamp = ros::Time::now();  // in order to better show the related graphs
+    msg.header.frame_id = "map";
+
+    state_pub.publish(msg);
+
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time::now();
+    marker.ns = ros::this_node::getNamespace();
+    marker.id = 0;
+    marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = x;
+    marker.pose.position.y = y;
+    marker.pose.position.z = 0;
+    tf::quaternionTFToMsg(q, marker.pose.orientation);
+    marker.scale.x = 1;
+    marker.scale.y = 1;
+    marker.scale.z = 1;
+    marker.color.a = 1.0; // alpha = transparence
+    marker.color.r = 1.0;
+    marker.color.g = 1.0;
+    marker.color.b = 1.0;
+    marker.mesh_resource = "package://tp3/meshs/boat.dae";
+    vis_pub.publish(marker);
+
+    transformStamped.header.stamp = ros::Time::now();
+    transformStamped.transform.translation.x = x;
+    transformStamped.transform.translation.y = y;
+    transformStamped.transform.rotation.x = q.x();
+    transformStamped.transform.rotation.y = q.y();
+    transformStamped.transform.rotation.z = q.z();
+    transformStamped.transform.rotation.w = q.w();
+    br.sendTransform(transformStamped);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+  }
+
+  return 0;
+}
