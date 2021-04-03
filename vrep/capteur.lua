3131e62599ec569a8aca718264d79f1d5b68d6b0
ngnepist <stephane.ngnepiepaye@ensta-bretagne.org>
2021-04-03 17:30:20 +0200

    petite modif sur la boussole

--- vrep/capteur.lua
+++ vrep/capteur.lua
@@ -1,10 +1,19 @@
+require ("math")
 
-function getCompass(objectName)
+function gaussian (mean, variance)
+    return  math.sqrt(-2 * variance * math.log(math.random())) *
+            math.cos(2 * math.pi * math.random()) + mean
+end
+
+function getCompass(objectName,motorON)
   -- This function get the value of the compass 
   objectHandle=sim.getObjectHandle(objectName)
   relTo = -1
   o=sim.getObjectOrientation(objectHandle,relTo)
-  heading = -0[3]   -- north along X > 0
-
+  if statemotor then -- if motor is ON 
+  	heading = -o[3] + gaussian(0,1)*math.pi  -- we add a gaussian noise
+  else
+  	heading = -o[3]   -- north along X > 0
+  end
   return heading
 end
